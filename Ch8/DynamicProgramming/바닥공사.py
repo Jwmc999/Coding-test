9a451c8074e656fc8732511f6757b84f556db406
Mervyn <66192214+Jwmc999@users.noreply.github.com>
2022-11-27 14:52:45 +0900

    Create 바닥공사.py

---
+++ Ch8/DynamicProgramming/바닥공사.py
@@ -0,0 +1,13 @@
+# Receive N
+n = int(input())
+
+# memoization
+d = [0] * 1001
+
+# Dynamic programming (bottom up)
+d[1] = 1
+d[2] = 3
+for i in range(3, n+1):
+  d[i] = (d[i-1] + 2 * d[i-2]) % 796796
+  
+print(d[n])
