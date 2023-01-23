6309ef9971880e730cff096827260e2c5b753719
Mervyn <66192214+Jwmc999@users.noreply.github.com>
2023-01-23 15:47:50 +0900

    Create 분수의 덧셈.py

---
+++ 사칙연산/분수의 덧셈.py
@@ -0,0 +1,5 @@
+from fractions import Fraction
+def solution(numer1, denom1, numer2, denom2):
+    f = Fraction(numer1, denom1) + Fraction(numer2, denom2)
+    answer = [f.numerator, f.denominator]
+    return answer
