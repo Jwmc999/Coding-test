f67fb3722165f80a9f1b8203d1616cc97938a101
Mervyn <66192214+Jwmc999@users.noreply.github.com>
2023-01-31 15:33:56 +0900

    Create 특이한 정렬.py

---
+++ 자료구조/특이한 정렬.py
@@ -0,0 +1,17 @@
+import heapq
+def solution(numlist, n):
+    numlst = [(num-n, abs(num-n)) for num in numlist]
+    numlist = [abs(num-n) for num in numlist]
+    numlst = sorted(numlst, key=lambda x: x[1])
+    answer = [] 
+    for num in numlst:
+        if numlist.count(num[1]) > 1:
+            answer.append(num[1])
+            answer.append(num[1]*-1)
+            numlst.remove(num)
+        elif num[0]<0:
+            answer.append(num[0])
+        else:
+            answer.append(num[0])
+    answer = [a+n for a in answer]
+    return answer
